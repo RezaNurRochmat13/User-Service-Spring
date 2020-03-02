cff5aeccaf2e86ae1ea244e3f47615daccd7acae
rezanurrochmat@gmail.com <rezanurrochmat@gmail.com>
2020-03-02 13:59:51 +0700

    Add global cors configuration

---
+++ src/main/java/com/backend/user/util/CorsConfiguration.java
@@ -0,0 +1,20 @@
+package com.backend.user.util;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebMvc
+public class CorsConfiguration implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedHeaders("*")
+                .allowedMethods("*");
+    }
+
+
+}
