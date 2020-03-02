2979ee9cb03fd910f968f3bbe4eb34ea4fb417de
rezanurrochmat@gmail.com <rezanurrochmat@gmail.com>
2020-03-02 14:21:21 +0700

    Refactor with automatic getter setter lombok

--- src/main/java/com/backend/user/module/users/domain/Users.java
+++ src/main/java/com/backend/user/module/users/domain/Users.java
@@ -2,12 +2,14 @@ package com.backend.user.module.users.domain;
 
 import com.fasterxml.jackson.databind.PropertyNamingStrategy;
 import com.fasterxml.jackson.databind.annotation.JsonNaming;
+import lombok.Data;
 
 import javax.persistence.*;
 
 @Entity
 @Table(name = "users_authentication")
 @JsonNaming(PropertyNamingStrategy.SnakeCaseStrategy.class)
+@Data
 public class Users {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -23,38 +25,4 @@ public class Users {
     @Column(name = "user_address")
     private String userAddress;
 
-    public Users() {
-    }
-
-    public String getUserName() {
-        return userName;
-    }
-
-    public void setUserName(String userName) {
-        this.userName = userName;
-    }
-
-    public Integer getUserAge() {
-        return userAge;
-    }
-
-    public void setUserAge(Integer userAge) {
-        this.userAge = userAge;
-    }
-
-    public String getUserAddress() {
-        return userAddress;
-    }
-
-    public void setUserAddress(String userAddress) {
-        this.userAddress = userAddress;
-    }
-
-    public Long getUserID() {
-        return userID;
-    }
-
-    public void setUserID(Long userID) {
-        this.userID = userID;
-    }
 }
